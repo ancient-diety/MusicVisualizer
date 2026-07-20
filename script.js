96e550ab2167e25b35c8f0414bfca604cf8d6f8d
Sable <izabelpetersen.leah@gmail.com>
2026-07-20 19:53:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -38,3 +38,29 @@ settingsButton.addEventListener("click", () => {
     settingsMenu.classList.toggle("hidden");
 
 });
+
+const themeButtons = document.querySelectorAll(".theme-button");
+
+themeButtons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        const theme = button.dataset.theme;
+
+        if (theme === "crimson") {
+
+            document.documentElement.style.setProperty("--bar-gradient",
+                "linear-gradient(to top, #700000, #ff3b3b)");
+
+        }
+
+        if (theme === "amethyst") {
+
+            document.documentElement.style.setProperty("--bar-gradient",
+                "linear-gradient(to top, #4b0082, #d38cff)");
+
+        }
+
+    });
+
+});
